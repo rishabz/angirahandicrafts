2ed7e18a1d0f92312c7447cca9577cd89df75d36
montu321 <montuverma30@gmail.com>
2024-05-12 21:31:03 +0530

    create login page and its functionality and middlewere but middlewere not work properly

---
+++ app/Http/Kernel.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    // protected $middleware = [
+    //     // Middleware classes that run on every HTTP request
+    //     // 'role' => \App\Http\Middleware\CheckRole::class,
+    // ];
+    
+    // protected $routeMiddleware = [
+    //     // Middleware classes that you can assign to routes
+    //     // 'auth' => \App\Http\Middleware\Authenticate::class,
+    //     'role' => \App\Http\Middleware\CheckRole::class,
+    // ];
+
+    // protected $middlewareGroups = [
+        // 'web' => [
+        //     // Other middleware...
+        //     \App\Http\Middleware\CheckRole::class,
+        // ],
+        // 'role' => [
+        //     \App\Http\Middleware\CheckRole::class,
+        // ],
+    // ];
+
+    protected $routeMiddleware = [
+        // 'auth' => \App\Http\Controller\Authenticate::class,
+        'role' => \App\Http\Middleware\Role::class,
+    ];
+    // protected $routeMiddleware = [
+    //     'role' => \App\Http\Middleware\RoleMiddleware::class,
+    // ];
+
+    // protected $middlewareGroups = [
+        // 'role' => [
+        //     CheckRole::class,
+        // ],
+    //     'web' => [
+    //         \Illuminate\Auth\Middleware\Authenticate::class,
+    //     ],
+    // ];
+    
+    // protected $routeMiddleware = [
+    //     'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+    // ];
+}
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
 
-// Route::get('/', function () {
-//     return view('index');
-// });
-
+// website routes
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/blog',[HomeController::class, 'blog']);
 Route::get('/blog/{title}',[HomeController::class, 'blogDetails']);
@@ -14,3 +12,21 @@ Route::get('/store',[HomeController::class, 'store']);
 Route::get('/about-us',[HomeController::class, 'aboutUs']);
 Route::get('/contact-us',[HomeController::class, 'contactUs']);
 Route::get('/shop',[HomeController::class, 'shop']);
+// website routes
+
+// login routes
+Route::get('/login',[AuthController::class,'index'])->name('login');
+Route::post('/login_submit',[AuthController::class,'login'])->name('login_submit');
+// login routes
+
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/dashboard',function(){
+        echo "Wellcome to dashboard.";
+    
+        // $user = Auth::user();
+        // echo $user->hasRole('Supper Admin');die();
+    
+        // Auth::logout();
+    })->name('dashboard');
+});
